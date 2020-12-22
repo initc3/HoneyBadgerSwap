7a90f0d8573b0ed788eb338ba78c081e24b04cad
lilione <lilioneviola@gmail.com>
2020-12-22 01:53:22 -0600

    test threshold

--- HoneyBadgerSwap/python/client/test.py
+++ HoneyBadgerSwap/python/client/test.py
@@ -1,25 +1,24 @@
-from utils import get_inverse, n, p, to_hex
+from utils import get_inverse, n, t, p, to_hex
 
-n = 4
-x, y = [1,2,3,4], \
-       [18457106183554705880258410793484639730509589330547761375057804664270278923797,
-        13349002587220538934323108005261136798800285149272711771854820196514835822696,
-        8240898990886371988387805217037633867090980967997662168651835728759392721595,
-        3132795394552205042452502428814130935381676786722612565448851261003949620494]
+x = []
+y = []
 
-# for i in range(n):
-#     file = f"Persistence/Transactions-P{i}.data"
-#     with open(file, 'rb') as f:
-#         x.append(i + 1)
-#         y.append(read(f, idx * sz))
-
-inputmask = 0
 for i in range(n):
-    tot = 1
-    for j in range(n):
-        if i == j:
-            continue
-        tot = tot * x[j] * get_inverse(x[j] - x[i]) % p
-    inputmask = (inputmask + y[i] * tot) % p
+    file = f"Player-Data/4-MSp-255/Randoms-MSp-P{i}"
+    with open(file, 'r') as f:
+        x.append(i + 1)
+        for line in f.readlines():
+            y.append(int(line))
+            break
 
-print(inputmask)
+for nn in range(t + 1, n + 1):
+    inputmask = 0
+    for i in range(nn):
+        tot = 1
+        for j in range(nn):
+            if i == j:
+                continue
+            tot = tot * x[j] * get_inverse(x[j] - x[i]) % p
+        inputmask = (inputmask + y[i] * tot) % p
+
+    print(inputmask)
