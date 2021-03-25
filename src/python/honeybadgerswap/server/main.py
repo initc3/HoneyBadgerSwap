d21c64425da1a919c474337cb2449c20010b7ed3
Sylvain Bellemare <sbellem@gmail.com>
2021-03-25 00:42:01 +0000

    wip

--- src/python/honeybadgerswap/server/main.py
+++ src/python/honeybadgerswap/server/main.py
@@ -16,9 +16,11 @@ from ..utils import (
 
 app = FastAPI()
 
+origins = ["https://jsfiddle.net", "https://fiddle.jshell.net"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
