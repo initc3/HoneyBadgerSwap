7bb51dc264744dc9e18dde12de60c7b634e14e60
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2022-03-22 17:16:14 +0800

    colAuction upd

--- ratel/src/python/colAuction/interact.py
+++ ratel/src/python/colAuction/interact.py
@@ -45,28 +45,6 @@ def inputAuction(appContract,colAuctionId,X,Amt,account):
         if status == 2:
             return
 
-def dutchAuctionSettle(appContract, colAuctionId, AmtToSell, StartPrice, LowestPrice, account):
-    idx = reserveInput(web3, appContract, 1, account)[0]
-    mask = asyncio.run(get_inputmasks(players(appContract), f'{idx}'))[0]
-    maskedStartPrice = (StartPrice + mask) % blsPrime
-
-    web3.eth.defaultAccount = account.address
-    tx = appContract.functions.dutchAuctionSettle(colAuctionId,AmtToSell,idx,maskedStartPrice,LowestPrice).buildTransaction({
-        'nonce': web3.eth.get_transaction_count(web3.eth.defaultAccount)
-    })
-    tx_hash = sign_and_send(tx, web3, account)
-    web3.eth.wait_for_transaction_receipt(tx_hash)
-
-    while True:
-        amtSold = appContract.functions.ret_amtSold(colAuctionId).call()
-        curPrice = appContract.functions.ret_curPrice(colAuctionId).call()
-        status = appContract.functions.status(colAuctionId).call()
-        if status == 3:
-            print('amtSold:', amtSold,' curPrice:', curPrice)
-            break
-        time.sleep(1)
-
-
 if __name__=='__main__':
     web3 = Web3(Web3.WebsocketProvider(url))
     web3.middleware_onion.inject(geth_poa_middleware, layer=0)
@@ -89,20 +67,20 @@ if __name__=='__main__':
     X2 = 5
     Amt2 = 10
     inputAuction(appContract,colAuctionId1,X2,Amt2,client_2)
+    print('finished input client_2')
 
     X3 = 3
     Amt3 = 6
     inputAuction(appContract,colAuctionId1,X3,Amt3,client_3)
+    print('finished input client_3')
     
     X4 = 7
     Amt4 = 7
     inputAuction(appContract,colAuctionId1,X4,Amt4,client_4)
+    print('finished input client_4')
 
     X5 = 2
     Amt5 = 9
     inputAuction(appContract,colAuctionId1,X5,Amt5,client_5)
+    print('finished input client_5')
 
-    AmtToSell1 = 20
-    StartPrice1 = 10
-    LowestPrice1 = 1 ###?
-    dutchAuctionSettle(appContract,colAuctionId1,AmtToSell1,StartPrice1,LowestPrice1,client_1)
